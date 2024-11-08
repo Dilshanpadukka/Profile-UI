3f2ec72f0b76180dc623edbbf9c3b0f7b6869506
Dilshanpadukka <udayankadilshan23@gmail.com>
2024-11-08 17:27:28 +0530

    create nav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:profile_ui/constants/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,38 +12,59 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColor.scaffoldBg,
       body: ListView(
+        scrollDirection: Axis.vertical,
         children: [
-          //Main Header
+          // Main Header
           Container(
-            height: 500,
+            height: 60,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
             width: double.maxFinite,
-            color: Colors.blueGrey,
-           
-         ),
-             //SKILS
+            decoration: BoxDecoration(
+              color: Colors.blueGrey, // Set the desired color here
+              gradient: LinearGradient(colors: [
+                Colors.transparent,
+                CustomColor.bgLight1,
+              ]),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Row(
+              children: [
+                Text("UDYANKA"),
+                Spacer(),
+                for (int i = 0; i < 5; i++)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                child:
+                  TextButton(
+                    onPressed: () {}, 
+                    child: Text("Button"),
+                    ),
+                ),
+              ],
+            ),
+          ),
+          // Skills
           Container(
             height: 500,
             width: double.maxFinite,
             color: const Color.fromARGB(255, 214, 230, 238),
-           
-         ),
-             //PROJECTS
+          ),
+          // Projects
           Container(
             height: 500,
             width: double.maxFinite,
             color: const Color.fromARGB(255, 131, 126, 204),
-           
-         ),    //CONTACT
+          ),
+          // Contact
           Container(
             height: 500,
             width: double.maxFinite,
             color: Colors.blueGrey,
-           
-         ),
+          ),
         ],
       ),
     );
   }
 }
-
